c608fe4d5646cc1288bb808d35686ae9443d3845
Rui Lopes <rgl@ruilopes.com>
2020-11-26 19:27:35 +0000

    upgrade dependencies and migrate to github actions

--- README.md
+++ README.md
@@ -1,33 +1,6 @@
-NOTICE: SUPPORT FOR THIS PROJECT ENDED ON 18 November 2020
-
-This projected was owned and maintained by Jet.com (Walmart). This project has reached its end of life and Walmart no longer supports this project.
-
-We will no longer be monitoring the issues for this project or reviewing pull requests. You are free to continue using this project under the license terms or forks of this project at your own risk. This project is no longer subject to Jet.com/Walmart's bug bounty program or other security monitoring.
-
-
-## Actions you can take
-
-We recommend you take the following action:
-
-  * Review any configuration files used for build automation and make appropriate updates to remove or replace this project
-  * Notify other members of your team and/or organization of this change
-  * Notify your security team to help you evaluate alternative options
-
-## Forking and transition of ownership
-
-For [security reasons](https://www.theregister.co.uk/2018/11/26/npm_repo_bitcoin_stealer/), Walmart does not transfer the ownership of our primary repos on Github or other platforms to other individuals/organizations. Further, we do not transfer ownership of packages for public package management systems.
-
-If you would like to fork this package and continue development, you should choose a new name for the project and create your own packages, build automation, etc.
-
-Please review the licensing terms of this project, which continue to be in effect even after decommission.
-
-ORIGINAL README BELOW
-
-----------------------
-
-[![Build status](https://ci.appveyor.com/api/projects/status/412auj8v5yxj1nx0/branch/master?svg=true)](https://ci.appveyor.com/project/Jet/damon/branch/master)
-
 # Damon
+
+[![Build status](https://github.com/rgl/damon/workflows/Build/badge.svg)](https://github.com/rgl/damon/actions?query=workflow%3ABuild)
 
 Damon is a supervisor program to constrain windows executables that are run under the `raw_exec` driver in Nomad.
 
@@ -82,7 +55,7 @@ Included with this repository is `make.ps1` which can be used to build `damon.ex
 
 ### Lint Code
 
-Runs [golangci-lint](https://github.com/golangci/golangci-lint) against the codebase. It will [Install golangci-lint](https://github.com/golangci/golangci-lint#local-installation) if it doesn't exist in `${GOPATH}/bin`.
+Runs [golangci-lint](https://github.com/golangci/golangci-lint) against the codebase. It will [Install golangci-lint](https://github.com/golangci/golangci-lint#local-installation) if it doesn't exist in the current directory.
 
 ```posh
 .\make.ps1 -Lint
--- make.ps1
+++ make.ps1
@@ -1,6 +1,6 @@
 #!powershell
 [CmdletBinding()]
-Param(    
+Param(
     [Parameter(ParameterSetName='Test')]
     [switch]$Test,
 
@@ -20,64 +20,69 @@ Param(
     [string]$OutFile = "damon.exe"
 )
 
-$GOLANG_LINT_VERSION="1.10.2"
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
+trap {
+    Write-Output "ERROR: $_"
+    Write-Output (($_.ScriptStackTrace -split '\r?\n') -replace '^(.*)$','ERROR: $1')
+    Exit 1
+}
+
+$GOLANG_LINT_VERSION="1.33.0"
 
 ## Setup
 $env:GOOS="windows"
 $env:GOARCH="amd64"
-$env:GO111MODULE="on"
 $env:GOFLAGS="-mod=vendor"
 
 ## Lint Code
 if ($Lint) {
     ## Install Linter
-    if( -not (Test-Path -Path $env:GOPATH\bin\golangci-lint.exe)) {
-        Mkdir -Path  $env:GOPATH\bin | Out-Null
-        Invoke-WebRequest -OutFile $env:GOPATH\bin\golangci-lint.zip -Uri "https://github.com/golangci/golangci-lint/releases/download/v${GOLANG_LINT_VERSION}/golangci-lint-${GOLANG_LINT_VERSION}-windows-amd64.zip"
-        Expand-Archive $env:GOPATH\bin\golangci-lint.zip -DestinationPath $env:GOPATH\bin
-        Move-Item -Path $env:GOPATH\bin\golangci-lint-${GOLANG_LINT_VERSION}-windows-amd64\*.exe -Destination $env:GOPATH\bin
+    if (-not (Test-Path -Path .\golangci-lint.exe)) {
+        Invoke-WebRequest -OutFile $env:TEMP\golangci-lint.zip -Uri "https://github.com/golangci/golangci-lint/releases/download/v${GOLANG_LINT_VERSION}/golangci-lint-${GOLANG_LINT_VERSION}-windows-amd64.zip"
+        Expand-Archive $env:TEMP\golangci-lint.zip $env:TEMP
+        Move-Item -Path $env:TEMP\golangci-lint-${GOLANG_LINT_VERSION}-windows-amd64\*.exe -Destination .
     }
     ## Run Linter
-    golangci-lint.exe run --exclude-use-default
+    .\golangci-lint.exe run --exclude-use-default
     exit $LASTEXITCODE
 }
 
 ## Run Test + Coverage
-if($Test) {
+if ($Test) {
     Write-Host "=== Test ==="
-    $env:TEST_EXE_PATH = "$env:ALLUSERSPROFILE\test-damon.exe"
+    $env:TEST_EXE_PATH = "$PWD\test-damon.exe"
     Write-Host "Compiling ${env:TEST_EXE_PATH}"
-    go.exe build -o $env:TEST_EXE_PATH ./testcmd/
-    if($env:APPVEYOR -eq "True") {
+    go.exe build -o $env:TEST_EXE_PATH -trimpath -ldflags="-s" ./testcmd/
+    if ($env:CI -eq "true") {
         $env:TEST_WIN32_USER_NAME="testuser"
         $env:TEST_WIN32_USER_PASSWORD="test123!"
         $user = Get-LocalUser -Name testuser -ErrorAction SilentlyContinue
-        if(-not $user) {
+        if (-not $user) {
             Write-Host "Create user $env:TEST_WIN32_USER_NAME"
             $password = ConvertTo-SecureString -AsPlainText -String $env:TEST_WIN32_USER_PASSWORD -Force
             New-LocalUser -Name $env:TEST_WIN32_USER_NAME -Password $password | Out-Null
             Write-Host "Assign 'Logon as Batch' rights"
-            Start-Process -FilePath $env:TEST_EXE_PATH -ArgumentList "batch_login",$env:TEST_WIN32_USER_NAME | Wait-Process  
+            Start-Process -FilePath $env:TEST_EXE_PATH -ArgumentList "batch_login",$env:TEST_WIN32_USER_NAME | Wait-Process
         }
-    } 
-    $v = if($VerboseOutput) { "-v" } else { "" }
+    }
+    $v = if ($VerboseOutput) { "-v" } else { "" }
     go.exe test $v -coverprofile $CoverProfile ./...
     exit $LASTEXITCODE
 }
 
 ## Run Build
-if($Build) {
+if ($Build) {
     $gitRevision = $(git rev-parse HEAD)
     $gitDescribe = $(git describe 2> $null)
     $buildTimestamp = $(Get-Date -UFormat "%Y-%m-%dT%T%Z")
-    
-    $ldflags = ""
-    $ldflags = "$ldflags -X github.com/jet/damon/version.GitCommit=${gitRevision}"
-    $ldflags = "$ldflags -X github.com/jet/damon/version.GitDescribe=${gitDescribe}"
-    $ldflags = "$ldflags -X github.com/jet/damon/version.BuildTime=${buildTimestamp}"
-    
-    Write-Host $gcflags
-    go.exe build -o $OutFile -ldflags="$ldflags" .
+
+    $ldflags = "-s"
+    $ldflags += " -X github.com/jet/damon/version.GitCommit=${gitRevision}"
+    $ldflags += " -X github.com/jet/damon/version.GitDescribe=${gitDescribe}"
+    $ldflags += " -X github.com/jet/damon/version.BuildTime=${buildTimestamp}"
+
+    go.exe build -o $OutFile -trimpath -ldflags="$ldflags"
     exit $LASTEXITCODE
 }
-
